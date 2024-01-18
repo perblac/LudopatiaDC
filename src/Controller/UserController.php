2cc598061835ca3e5d3ac2e2537aa592684dcf11
perblac <yhana.kromx@gmail.com>
2024-01-18 15:57:00 +0100

    limitar el crud de User sólo para admins

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -10,7 +10,9 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
 
+#[IsGranted('ROLE_ADMIN')]
 #[Route('/user')]
 class UserController extends AbstractController
 {
