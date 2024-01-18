5d9194c54dcd1c08f70a1f4cbcf96b115601c5e5
perblac <yhana.kromx@gmail.com>
2024-01-18 15:55:08 +0100

    poner seleccion de roles en el Form de User

--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,6 +6,7 @@ use App\Entity\User;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 
 class UserType extends AbstractType
 {
@@ -13,7 +14,15 @@ class UserType extends AbstractType
     {
         $builder
             ->add('username')
-            ->add('roles')
+            // ->add('roles')
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'Admin' => 'ROLE_ADMIN',
+                    'User' => 'ROLE_USER',
+                ],
+                'multiple' => true,
+                'expanded' => false,
+            ])
             ->add('password')
         ;
     }
